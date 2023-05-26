d7b16bc3c2e021282aff5011855e48bcb11dc9ec
Nicolas Olmos <nicolas.nos@hotmail.com>
2023-05-26 16:12:33 -0500

    header arreglado

--- wikideas/src/Pages/App.jsx
+++ wikideas/src/Pages/App.jsx
@@ -2,12 +2,14 @@ import { useRoutes, BrowserRouter } from "react-router-dom";
 import Home from "../components/Home";
 import Create from "../components/Create";
 import Team from "../components/Team";
+import Contact from "../components/Contact";
 
 const AppRoutes = () => {
   let routes = useRoutes([
     { path: "/", element: <Home /> },
     { path: "/create", element: <Create /> },
     { path: "team", element: <Team /> },
+    { path: "contact", element: <Contact /> },
   ]);
   return routes;
 };
--- wikideas/src/components/Footer.jsx
+++ wikideas/src/components/Footer.jsx
@@ -1,18 +1,17 @@
-import '../css/Footer.css';
+import "../css/Footer.css";
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer_content">
-        <p className="footer-text">Creado por y para la comunidad</p>
-        <div className="footer-icons">
-          <i className="fab fa-facebook"></i>
-          <i className="fab fa-twitter"></i>
-          <i className="fab fa-instagram"></i>
-        </div>
+      <p className="footer-text">Creado por y para la comunidad</p>
+      <a href="/contact">Contactate con nosotros</a>
+      <div className="footer-icons">
+        <i className="fab fa-facebook"></i>
+        <i className="fab fa-twitter"></i>
+        <i className="fab fa-instagram"></i>
       </div>
     </footer>
   );
-}
+};
 
 export default Footer;
