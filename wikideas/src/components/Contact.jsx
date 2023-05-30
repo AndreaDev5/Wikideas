ec63dd39e7ee887879a3230127cf1328250dc7d2
AndreaDev5 <andreazuluagamonroy555@gmail.com>
2023-05-30 15:28:14 -0500

    form-responsive

--- wikideas/src/components/Contact.jsx
+++ wikideas/src/components/Contact.jsx
@@ -1,4 +1,4 @@
-import  { useState } from 'react';
+import { useState } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import '../css/Contact.css';
@@ -10,39 +10,54 @@ export default function Contact() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    
     console.log('Nombre:', name);
     console.log('Email:', email);
     console.log('Mensaje:', message);
+
     setName('');
     setEmail('');
     setMessage('');
   };
 
   return (
-    <section className="contact-container">
+    <>
       <Header />
-      <div className="form-container">
-        <h2>Formulario de Contacto</h2>
-        <form onSubmit={handleSubmit}>
-          <label>
-            Nombre:
-            <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-          </label>
-          <br />
-          <label>
-            Email
-            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-          </label>
-          <br />
-          <label>
-            Mensaje
-            <textarea value={message} onChange={(e) => setMessage(e.target.value)} />
-          </label>
-          <br />
-          <button type="submit">Enviar</button>
-        </form>
-      </div>
+      <section className="contact-container">
+        <div className="form-container">
+          <h2>Formulario de Contacto</h2>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="name">Nombre</label>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+
+            <label htmlFor="message">Mensaje</label>
+            <textarea
+              id="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              required
+            ></textarea>
+
+            <button type="submit">Enviar</button>
+          </form>
+        </div>
+      </section>
       <Footer />
-    </section>
+    </>
   );
 }
--- wikideas/src/components/Footer.jsx
+++ wikideas/src/components/Footer.jsx
@@ -3,13 +3,13 @@ import "../css/Footer.css";
 const Footer = () => {
   return (
     <footer className="footer">
-      <p className="footer-text">Creado por y para la comunidad</p>
-      <a href="/contact">Contactate con nosotros</a>
       <div className="footer-icons">
         <i className="fab fa-facebook"></i>
         <i className="fab fa-twitter"></i>
         <i className="fab fa-instagram"></i>
       </div>
+      <p className="footer-text">Creado por y para la comunidad</p>
+      <a href="/contact">Contactate con nosotros</a>
     </footer>
   );
 };
